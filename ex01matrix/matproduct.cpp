09bde8c9665c7e887a891efecf81af3e6ef6a7a2
Arkadiusz Gabryś <arkadiuszgabrys@gmail.com>
2014-11-05 23:43:15 +0100

    Komentarze do kodu

--- ex01matrix/matproduct.cpp
+++ ex01matrix/matproduct.cpp
@@ -8,14 +8,21 @@ using std::cin;
 using std::endl;
 using std::exception;
 
+/*
+    Matrix - can store double numbers.
+*/
 class Matrix
 {
 private:
-    double** matrix;
-    unsigned int rowsCount;
-    unsigned int columsCount;
+    double** matrix;            // pointer to the 2dim array
+    unsigned int rowsCount;     // number of rows
+    unsigned int columsCount;   // number of columns
 
 public:
+
+    /*
+        Consturctor - the rows and columns number must be specified.
+    */
     Matrix(unsigned int _rowsCount, unsigned int _columnsCount, double defaultValue = 0.0) : rowsCount(_rowsCount), columsCount(_columnsCount)
     {
         if (rowsCount <= 0 || columsCount <= 0)
@@ -30,6 +37,9 @@ public:
                 matrix[r][c] = defaultValue;
     }
 
+    /*
+        Copy constructor
+    */
     Matrix(Matrix& m) : rowsCount(m.rowsCount), columsCount(m.columsCount)
     {
         matrix = new double*[rowsCount];
@@ -41,6 +51,9 @@ public:
                 matrix[r][c] = m[r][c];
     }
 
+    /*
+        Destructor
+    */
     ~Matrix()
     {
         for (unsigned int i = 0; i < rowsCount; i++)
@@ -48,15 +61,25 @@ public:
         delete[] matrix;
     }
 
+    /*
+        Getters for matrix dimensions
+    */
     unsigned int getRowsCount() const { return rowsCount; }
     unsigned int getColumnsCount() const { return columsCount; }
 
+    /*
+        Access opeartor
+    */
     double* operator[](unsigned int row) { return matrix[row]; }
 
+    /*
+        Assign operator
+    */
     Matrix& operator= (Matrix& m)
     {
-        this->~Matrix();
+        this->~Matrix(); // clear memory
 
+        // copy data
         rowsCount = m.rowsCount;
         columsCount = m.columsCount;
 
@@ -71,11 +94,16 @@ public:
         return (*this);
     }
 
+    /*
+        Multiplication operator
+    */
     Matrix operator*(Matrix& m)
     {
+        // number of columns of the first matrix must be equal to the number of rows of the second one
         if (columsCount != m.getRowsCount())
             throw exception("Matrices can not be multiplied. Number of collumns of the first matrix must be equla to number of rows of the second one!");
 
+        // create new matrix
         Matrix result(rowsCount, m.getColumnsCount());
 
         for (unsigned int r = 0; r < result.getRowsCount(); r++)
@@ -91,6 +119,7 @@ public:
     }
 };
 
+// filling matrix with data
 void fillMatrix(Matrix& m)
 {
     for (unsigned int r = 0; r < m.getRowsCount(); r++)
@@ -104,6 +133,7 @@ void fillMatrix(Matrix& m)
     }
 }
 
+// to write matrix content
 void writeMatrix(Matrix& m)
 {
     for (unsigned int r = 0; r < m.getRowsCount(); r++)
@@ -116,12 +146,13 @@ void writeMatrix(Matrix& m)
 
 int main(int argv, char* argc[])
 {
-    int s1, s2, s3;
+    int s1, s2, s3; // for matrix dimensions
     
     cout << "s1: "; cin >> s1;
     cout << "s2: "; cin >> s2;
     cout << "s3: "; cin >> s3;
 
+    // create and fill two matrixes
     Matrix m1(s1, s2);
     Matrix m2(s2, s3);
 
@@ -132,9 +163,11 @@ int main(int argv, char* argc[])
     cout << endl;
     cout << "m2[" << s1 << "][" << s2 << "]:" << endl << endl;
     fillMatrix(m2);
+    // ----------------------------
 
-    Matrix m3 = m1 * m2;
+    Matrix m3 = m1 * m2; // multiply two matrices to obtain third one
 
+    // show the result
     cout << "m3 = m1 * m2" << endl;
     writeMatrix(m3);
 
